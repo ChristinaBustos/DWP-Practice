a49107ff50c2a6364eb3e35fa144b24bb84db3d9
CarlosGmz-30 <20223tn066@utez.edu.mx>
2024-01-09 19:25:30 -0600

    Inicio de Vue

---
+++ Client/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div id="app">
+   <b-button>Button</b-button>
+  <b-button :disabled="false" variant="danger">Button</b-button>
+  <b-button variant="success">Button</b-button>
+  <b-button variant="outline-primary">Button</b-button>
+  </div>
+</template>
+
+<style scoped>
+
+
+</style>
