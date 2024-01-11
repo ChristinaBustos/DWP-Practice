6c864294154a8d41f81cf4b5adbf24f046ccbcf9
Cristina <20213tn151@utez.edu.mx>
2024-01-10 22:53:03 -0600

    [UPDATE] new change

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -5,7 +5,7 @@
 <template>
   <div id="app">
    <b-button>Button</b-button>
-  <b-button :disabled="false" variant="danger">Button</b-button>
+  <b-button :disabled="false" variant="danger">Hola</b-button>
   <b-button variant="success">Button</b-button>
   <b-button variant="outline-primary">Button</b-button>
   </div>
